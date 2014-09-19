538598bb118d4bc1a6dfbd2c55bab1cbfc759872
Leon van Bokhorst <leon@remondo.net>
2014-09-19 19:12:18 +0200

    Lesson 6 PS

---
+++ CS01-Cumulative-RemoveTags.py
@@ -0,0 +1,43 @@
+# Question 4: Remove Tags
+
+# When we add our words to the index, we don't really want to include
+# html tags such as <body>, <head>, <table>, <a href="..."> and so on.
+
+# Write a procedure, remove_tags, that takes as input a string and returns
+# a list of words, in order, with the tags removed. Tags are defined to be
+# strings surrounded by < >. Words are separated by whitespace or tags. 
+# You may assume the input does not include any unclosed tags, that is,  
+# there will be no '<' without a following '>'.
+
+
+def remove_tag(body, starttag, stoptag):
+    return body[: starttag] + ' ' + body[stoptag + 1:]
+
+
+def remove_tags(body):
+    while True:
+        starttag = body.find('<')
+        stoptag = body.find('>')
+
+        if starttag == -1:
+            break
+
+        body = remove_tag(body, starttag, stoptag)
+
+    return body.split()
+
+
+print remove_tags('''<h1>Title</h1><p>This is a
+                    <a href="http://www.udacity.com">link</a>.<p>''')
+# >>> ['Title','This','is','a','link','.']
+
+print remove_tags('''<table cellpadding='3'>
+<tr><td>Hello</td><td>World!</td></tr>
+                    </table>''')
+# >>> ['Hello','World!']
+
+print remove_tags("<hello><goodbye>")
+#>>> []
+
+print remove_tags("This is plain text.")
+#>>> ['This', 'is', 'plain', 'text.']
